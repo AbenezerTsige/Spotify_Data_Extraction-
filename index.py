fe26c96aec330d7fb028b656d072f78d3224b409
Abenezertsige <Abelnamaga@gmail.com>
2022-07-13 15:22:07 -0500

    uploading index.py which extracts track name, main artist,  popularity, genre, album

---
+++ index.py
@@ -0,0 +1,44 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pprint 
+
+with open(r"secret.txt") as f:
+        secret_ls = f.readlines()
+        cid = secret_ls[0][:-2]
+        secret = secret_ls[1]
+        f.close()
+
+cid = cid.strip()
+secret = secret.strip()
+
+#Authentication - without user
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+    #URI
+    track_uri = track["track"]["uri"]
+    
+    #Track name
+    track_name = track["track"]["name"]
+    
+    #Main Artist
+    artist_uri = track["track"]["artists"][0]["uri"]
+    artist_info = sp.artist(artist_uri)
+    
+    #Name, popularity, genre
+    artist_name = track["track"]["artists"][0]["name"]
+    artist_pop = artist_info["popularity"]
+    artist_genres = artist_info["genres"]
+    
+    #Album
+    album = track["track"]["album"]["name"]
+    
+    #Popularity of the track
+    track_pop = track["track"]["popularity"]
+
+pprint.pprint(sp.audio_features(track_uri)[0])
