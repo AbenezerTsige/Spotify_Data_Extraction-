27ea1dbcbe491b240c21c8677bd0e734d6c2dc2f
Abenezertsige <Abelnamaga@gmail.com>
2022-08-08 19:22:40 -0500

    Updated into jupyternotebook with a bar plot for longest songs in the track list

---
+++ cred.py
@@ -0,0 +1,4 @@
+import os
+os.environ["SPOTIPY_CLIENT_ID"] ='26816597e67c4cdbbbdd3972167a7006'
+os.environ["SPOTIPY_CLIENT_SECRET"] ='7036b0016a7948ce83d583733bb415d2'
+os.environ["SPOTIPY_REDIRECT_URI"] ='http://localhost:8888/callback/'
